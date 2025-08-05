3f90c823619a23b48246e09b9607bd3698882448
luana-gaarcia <luanagm09@gmail.com>
2025-08-05 11:05:14 -0300

    Primeiro envio

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+const app = express()
+
+app.use(express.json())
+
+const usuarios = []
+
+// ROTAS
+app.get('/cadastro',(req,res)=>{
+    //res.send('DEU CERTO COM O GET')
+    res.status(200).send(usuarios)
+})
+
+app.post('/cadastro',(req,res)=>{
+    //console.log(req.body)
+    usuarios.push(req.body)
+   res.status(201).send('DEU CERTO COM O POST')
+   
+})
+
+
+// PORTA LOCAL DO SERVIDOR
+app.listen(3000,()=>{
+    console.log('SERVIDOR ESTÁ RODANDO')
+})
